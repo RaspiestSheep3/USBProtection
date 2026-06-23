af4f2fd6d305559c4bbf8fa3e9279c1f0a0d5245
RaspiestSheep3 <raspiestsheep3@hotmail.com>
2026-06-23 15:24:08 +0100

    Initial Commit

---
+++ USBProtection.cpp
@@ -0,0 +1,12 @@
+﻿// USBProtection.cpp : Defines the entry point for the application.
+//
+
+#include "USBProtection.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
